9584dde34e36a7ee68a6cf6a2f1fc60315204f02
CSD-business <143720980+CSD-business@users.noreply.github.com>
2025-02-24 11:37:05 -0500

    Clean up code from main to ClickableOptions

--- Scripts/ClickableOptions.gd
+++ Scripts/ClickableOptions.gd
@@ -1,11 +1,28 @@
 extends VBoxContainer
 
+@export var Main : Node
 
 # Called when the node enters the scene tree for the first time.
+#For now, this is here in ready().
+#This checks (at some point) all of the child nodes
+#And connects to any objects clicked signal
+#So that we can do some code when they are clicked
 func _ready():
-	pass # Replace with function body.
+	print(Main.get_children())
+	for node in Main.get_children():
+		if node.is_in_group("Item"):
+			node.get_node("Clickable").is_clicked.connect(_is_clicked)
+			node.get_node("Clickable").isnt_clicked.connect(_isnt_clicked)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _is_clicked():
+	self.show()
+	self.position = get_global_mouse_position()
+func _isnt_clicked():
+	self.hide()
+#Some code that when the object is selected it gets fed
+#the info from the object (inspect text, can it be taken- dialogue?)
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -4,23 +4,14 @@ var is_options_showing
 
 # Called when the node enters the scene tree for the first time.
 
-#For now, this is here in ready().
-#This checks (at some point) all of the child nodes
-#And connects to any objects clicked signal
-#So that we can do some code when they are clicked
+
 func _ready():
-	print(self.get_children())
-	for node in self.get_children():
-		if node.is_in_group("Item"):
-			node.get_node("Clickable").is_clicked.connect(_is_clicked)
-			node.get_node("Clickable").isnt_clicked.connect(_isnt_clicked)
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
 	
 #Code that runs when the mouse clicks on a clickable object
-func _is_clicked():
-	$ClickableOptions.show()
-	$ClickableOptions.position = get_global_mouse_position()
-func _isnt_clicked():
-	$ClickableOptions.hide()
+#On second thought this shouldn't be in main so move it to the 
+#clickable options
+
